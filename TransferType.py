906bc2852da4591f4a516333f2f5e5a4c158f006
markbejosano <mbejosano@west.com>
2017-05-17 09:44:41 -0500

    updates

--- TransferType.py
+++ TransferType.py
@@ -6,31 +6,23 @@ import csv
 failed = 0
 def main():
     try:
-        file = os.stat("transfertype.txt")
+        file = os.stat("Data File.csv")
         if file.st_size == 0:
-            print "transfertype.txt is Empty"
+            print "Data File is Empty"
     except OSError:
-        print "transfertype.txt not Found"
+        print "Data File.csv not Found"
         sys.exit(1)
 
-    try:
-        file = os.stat("Calllog.txt")
-        if file.st_size == 0:
-            print "Calllog.txt is Empty"
-            sys.exit(1)
-    except OSError:
-        print "Calllog.txt not Found"
-        sys.exit(1)
-
-def compare(calllog1, transfertype1, y):
+def compare(calllog1, transfertype1, y, gen_Report):
     x = 0
+    z = 0
     transfer = transfertype1.split(';')
     total_terms = len(transfer)
     global failed
     y = str(y)
-    # print total_terms
     print "Test " + y + ": " + "For Call log " + calllog1
     total_terms = total_terms -1
+    alllines = set()
     while total_terms > x :
         flag = 0
         split_1 = transfer[x].split('=')
@@ -41,48 +33,63 @@ def compare(calllog1, transfertype1, y):
             for line in calllog:
                 if (first +':' in line.lower() or first +'=' in line.lower()) and second in line:
                     verbiage.add(line)
+                    alllines.add(line)
                     flag = 1
                 elif first +':' in line.lower() or first +'=' in line.lower():
                     verbiage.add(line)
-
-
+                    alllines.add(line)
 
         if flag == 0:
             print transfer[x] + "\tWrong Value"
+            z = 1
         elif len(verbiage)==0:
             print transfer[x] + "\tNot Found"
+            z = 1
         else:
             print transfer[x] + "\tFound"
         print ''.join(verbiage)
         x += 1
+    verbi = '<br/>'.join(alllines)
+    prompi = '<br/>'.join(transfer)
+    if z == 1:
+        print "                   STATUS: FAILED"
+        gen_Report.write(
+            "<tr><td>" + y + "</td><td>" + prompi + "</td> <td>" + verbi + "</td> <td bgcolor='#e06745'>Failed</td>  <td>" + calllog1 + "</td></tr>")
+
+    else:
+        print "                   STATUS: PASSED"
+        gen_Report.write(
+            "<tr><td>" + y + "</td><td>" + prompi + "</td> <td>" + verbi + "</td> <td bgcolor='#99e26f'>Passed</td>  <td>" + calllog1 + "</td></tr>")
+
 
 def excel():
+    gen_Report = open("report.html", "a")
+    # gen_Report.write("<html><table align ='center'"
+    # "style='background-color:orange;'> "
+    # "<style>table, th, td {border: 1px solid black;}</style>"
+    # "<h1 >Build Acceptance Test</h1><p>Call Flow</p>")
+    gen_Report.write("<html><table align ='center' bgcolor='orange' border='1' width='70%'> <h1>Build Acceptance Test</h1> <p>Transfer Term</p>")
     z= 0
     with open('Data File.csv', 'rb') as f:
         reader = csv.reader(f)
         next(reader, None)
-        # flow =[]
         y= 1;
+        gen_Report.write("<tr><td> Test Case </td> "
+                         "<td> Transfer Term </td> "
+                         "<td> Actual Logs </td> "
+                         "<td> Result </td> "
+                         "<td>Call Log </td></tr>")
         for line in reader:
             transfertype1 =line[1]
             calllog1 = line[2]
-            # print transfertype
-            # print len(transfertype)
             if len(transfertype1) >= 5:
                 if len(calllog1)>= 5:
-                    # print transfertype1
-                    # print calllog1
-                    compare(calllog1, transfertype1, y)
+                    compare(calllog1, transfertype1, y, gen_Report)
                     y+=1
-
-            # print transfertype
-            # print calllog
-            # print flow[0]
 
 if __name__ == "__main__":
 
-    # main()
-    # compare()
+    main()
     excel()
     if failed == 1:
         print"\n\n\n"
