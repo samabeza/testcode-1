6083fb31599c121068bcb875c0810dc6e42810f6
rmgamata <rgamatan@west.com>
2017-05-18 10:30:35 -0500

    Update TransferType.py

--- TransferType.py
+++ TransferType.py
@@ -13,7 +13,7 @@ def main():
         print "Data File.csv not Found"
         sys.exit(1)
 
-def compare(calllog1, transfertype1, y, gen_result):
+def compare(calllog1, transfertype1, y, gen_result, gen_report):
     x = 0
     z = 0
     transfer = transfertype1.split(';')
@@ -55,33 +55,41 @@ def compare(calllog1, transfertype1, y, gen_result):
     if z == 1:
         print "                   STATUS: FAILED"
         gen_result.write(
-            "<tr><td>" + y + "</td><td>" + prompi + "</td> <td>" + verbi + "</td> <td bgcolor='#e06745'>Failed</td>  <td>" + calllog1 + "</td></tr>")
-
+            "<tr><td align='center'>" + y + "</td><td>" + prompi + "</td> <td>" + verbi + "</td> <td bgcolor='#e06745'>Failed</td>  <td>" + calllog1 + "</td></tr>")
+        gen_report.write(
+            "<tr><td align='center'>" + y + "</td><td>" + calllog1 + "</td> <td bgcolor='#e06745'>Failed </td></tr>")
     else:
         print "                   STATUS: PASSED"
         gen_result.write(
             "<tr><td>" + y + "</td><td>" + prompi + "</td> <td>" + verbi + "</td> <td bgcolor='#99e26f'>Passed</td>  <td>" + calllog1 + "</td></tr>")
-
+        gen_report.write(
+            "<tr><td align='center'>" + y + "</td><td>" + calllog1 + "</td> <td bgcolor='#99e26f'>Passed </td></tr>")
 
 def excel():
     gen_result = open("result.html", "a")
-    gen_result.write("<html><table align ='center' border='1' width='70%'> <h1>Build Acceptance Test</h1> <p>Transfer Term</p>")
+    gen_report = open("report.html", "a")
+    gen_result.write("<html><table align ='center' border='1' width='80%'> <center> <h1>Build Acceptance Test</h1> <h3>Transfer Term</h3></center></table>")
+    gen_report.write("<html><table align='center' border='1' width='80%'> <center> <h1>Build Acceptance Test</h1> <h3>Transfer Term</h3></center></table>")
     z= 0
     with open('Data File.csv', 'rb') as f:
         reader = csv.reader(f)
         next(reader, None)
         y= 1;
-        gen_result.write("<tr><td> Test Case </td> "
+        gen_result.write("<table border='1' align='center'> <tr><td> Test Case </td> "
                          "<td> Transfer Term </td> "
                          "<td> Actual Logs </td> "
                          "<td> Result </td> "
+                         "<td> Remarks </td> "
                          "<td>Call Log </td></tr>")
+      	gen_report.write("<br/> <table border='1' align='center'> <tr><td align='center'> Test Case </td> " 
+                         "<td align='center'> Call Log </td>"
+                         "<td align='center'> Pass/Fail </td></tr>")
         for line in reader:
             transfertype1 =line[1]
             calllog1 = line[2]
             if len(transfertype1) >= 5:
                 if len(calllog1)>= 5:
-                    compare(calllog1, transfertype1, y, gen_result)
+                    compare(calllog1, transfertype1, y, gen_result, gen_report)
                     y+=1
 
 if __name__ == "__main__":
