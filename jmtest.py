8f3f53314524330019cfce004b9d3a6c55098307
rmgamata <rgamatan@west.com>
2017-05-19 11:13:12 -0500

    Update jmtest.py

--- jmtest.py
+++ jmtest.py
@@ -4,7 +4,7 @@ y = os.path.abspath("./HTTP Request.jmx")
 z = y.replace('\\','\\\\') 
 
 
-initial_path = "jmeter -J jmeter.save.saveservice.output_format=xml -n -t"
+initial_path = "jmeter -Jjmeter.save.saveservice.output_format=xml -n -t"
 command = "-l HTTPRequest.jtl"
 print "PATH: ", y
 print "INITIAL_PATH: ", initial_path
