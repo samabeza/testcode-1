e35432834e193707519a284c831e3efb41f54b4f
rmgamata <rgamatan@west.com>
2017-05-24 12:00:32 -0500

    Update CallFlow.py

--- CallFlow.py
+++ CallFlow.py
@@ -104,9 +104,9 @@ def excel():
 						 "<td align='center' bgcolor='#c2c4c6'> <b>Call Log </b></td> "
 				 		 "<td align='center' bgcolor='#c2c4c6'> <b>Remarks </b></td>"
 						 "<td align='center' bgcolor='#c2c4c6'> <b>Result </b></td></tr>")
-		gen_report.write("<br/><table border='1' align='center' width='50%'><tr><td align='center' bgcolor='#c2c4c6'> <b>Test Case</b> </td> "
-						 "<td align='center' bgcolor='#c2c4c6'> <b>Call Log</b> </td> "
-						 "<td align='center' bgcolor='#c2c4c6'> <b>Pass/Fail </b></td></tr>")
+		gen_report.write("<br/><table border='1' align='center' width='35%'><tr><td align='center' bgcolor='#c2c4c6' width='10%'> <b>Test Case</b> </td> "
+						 "<td align='center' bgcolor='#c2c4c6' width='60%'> <b>Call Log</b> </td> "
+						 "<td align='center' bgcolor='#c2c4c6' width='20%'> <b>Pass/Fail </b></td></tr>")
 		for line in reader:
 			callflow1 =line[0]
 			calllog1 = line[2]
