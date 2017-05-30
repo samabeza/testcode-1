5db3e5dea373b818a4cccf8f1ec3445c32c8c367
rmgamata <rgamatan@west.com>
2017-05-30 10:09:18 -0500

    Update TransferType.py

--- TransferType.py
+++ TransferType.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
         percentage = Decimal(overall_passed) / Decimal(testcases) * 100
         totalper = str(percentage) + '%'
         gen_report.write(
-            "</table><br/><table align='center'> <tr><td><h2>Passed: " + totalper + "</h2></td></tr> </table>")
+            "</table><table align='center'> <tr><td><h3>Passed: " + totalper + "</h3></td></tr> </table>")
     if failed == 1:
         print"\n\n\n"
         raise SystemError('One of the Test Cases Failed')
