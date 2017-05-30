18f1142cf8eab711341a6217864997f3e8ddf0c2
rmgamata <rgamatan@west.com>
2017-05-30 11:08:05 -0500

    Update TransferType.py

--- TransferType.py
+++ TransferType.py
@@ -93,7 +93,7 @@ def excel():
     gen_report = open("report.html", "a")
     gen_result.write("<html> <center><h1>Build Acceptance Test</h1> <h3>Transfer Term</h3></center>")
     gen_report.write("<html><table align='center' border='1' width='70%'> </table>")
-    gen_report.write("<br/><table align='center' width='35%'><tr><td><font size='6'><b>Transfer Term</b></font></td></tr></table>")
+    gen_report.write("<br/><table align='center' width='35%'><tr><td align='center'><font size='4'><b>Transfer Term</b></font></td></tr></table>")
     z = 0
     with open('Data File.csv', 'rb') as f:
         reader = csv.reader(f)
