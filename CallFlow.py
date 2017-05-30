03d25ce5307aec84d4c1e3d984b550609f7df68c
rmgamata <rgamatan@west.com>
2017-05-30 10:34:00 -0500

    Update CallFlow.py

--- CallFlow.py
+++ CallFlow.py
@@ -99,7 +99,7 @@ def excel():
 	
 	gen_result = open("Call Flow Result.html", "a")
 	gen_report = open("report.html", "a")
-	gen_result.write("<html> <center><h1>Build Acceptance Test</h1> <h3>Call Flow</center>")
+	gen_result.write("<html> <center><h1>Build Acceptance Test</h1> <h3>Call Flow</h3></center>")
 	gen_report.write("<html><table align ='center'  border='1' width='70%'> <center><h1>Build Acceptance Test</h1><br/></table> <center><h3>Call Flow</h3></center> ")
 	z= 0
 	with open('Data File.csv', 'rb') as f:
