710e08b0e50684d819cdfe3df70687371f3cb292
rmgamata <rgamatan@west.com>
2017-05-30 11:27:00 -0500

    Update TransferType.py

--- TransferType.py
+++ TransferType.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
         gen_report.write(
             "</table><table align='center'> <tr><td><h3>Passed: " + totalper + "</h3></td></tr> </table>")
         writetime = open("writetime.txt","a")
-        writetime.write("TransferType Percentage= " + percentage + "\n")
+        writetime.write("TransferType Percentage= " + totalper + "\n")
     if failed == 1:
         print"\n\n\n"
         raise SystemError('One of the Test Cases Failed')
