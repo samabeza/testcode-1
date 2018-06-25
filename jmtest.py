1480763ec28dae81f9791de057443ab9dfc4648a
markbejosano <mbejosano@west.com>
2018-06-25 15:39:57 -0500

    Update jmtest.py

--- jmtest.py
+++ jmtest.py
@@ -2,13 +2,14 @@ import os
 import os.path
 
 check_file = os.path.isfile("HTTPRequest.jmx")
+print check_file
+check_file = str(check_file)
 if check_file=="True":
   y = os.path.abspath("./HTTPRequest.jmx")
   z = y.replace('\\','\\\\') 
 
   initial_path = "jmeter -Jjmeter.save.saveservice.output_format=xml -n -t"
   command = "-l HTTPRequest.jtl"
-
 
   final = initial_path + " " + z + " " + command
 
