27a251a817796497bd086e8ed1185a6ef679a130
rmgamata <rgamatan@west.com>
2018-08-01 17:08:34 -0500

    Update testparam.py

--- testparam.py
+++ testparam.py
@@ -1,24 +1,9 @@
 import sys
-import os
-import glob
-import subprocess
-import csv
-from decimal import *
-from datetime import datetime
 
-def main(foldername):
-    try:
-        file = os.stat(foldername+"/Data File.csv")
-        if file.st_size == 0:
-            print "Data File is Empty"
-    except OSError:
-        print "Data File.csv not Found"
-        sys.exit(1)
+def main():
+    # print command line arguments
+    for arg in sys.argv[1:]:
+        print arg
 
 if __name__ == "__main__":
-	if len(sys.argv)==0:
-		print("please pass folder name as argument")
-		exit()
-	foldername=sys.argv[1]
-	print('foldername==>{}'.format(foldername) )
-	main(foldername)
+    main()
