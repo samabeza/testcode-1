31f2e573bbc519780341c2c026332692c3eccbe4
rmgamata <rgamatan@west.com>
2018-08-01 17:23:53 -0500

    Update testparam.py

--- testparam.py
+++ testparam.py
@@ -11,6 +11,5 @@ if __name__ == "__main__":
             print("Please pass foldername as argument")
             exit()
     foldername=sys.argv[1]
-    print foldername
     main(foldername)
     
